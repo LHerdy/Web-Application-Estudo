de73ebcb7d1aa270a7e87d45e2eea9fe2ff6b67b
Leandro Herdy <le.herdy@gmail.com>
2020-06-02 04:04:22 -0300

    Implementação dos templates menu e contact.

--- app_estudo/settings.py
+++ app_estudo/settings.py
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'br-pt'
 
-TIME_ZONE = 'America/Sap_Paulo'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -121,4 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'app_estudo/static')
+]
+
